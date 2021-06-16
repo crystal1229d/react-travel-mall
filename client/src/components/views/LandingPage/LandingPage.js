bc4ad24cef422a5895fd1e0382eb4e2d913ba766
crystal1229d <crystal1229d@naver.com>
2021-06-16 22:45:39 +0900

    add landing page which shows all products

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,26 @@
+import Axios from 'axios'
 import React from 'react'
-import { FaCode } from "react-icons/fa";
+import { useEffect } from 'react'
 
 function LandingPage() {
+
+    useEffect(() => {
+    
+        Axios.post('/api/product/products')
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response.data)
+                } else {
+                    alert('상품들을 가져오는데 실패했습니다');
+                }
+            })
+
+    }, [])
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+        <div>
+            Langindg Page
+        </div>
     )
 }
 
--- server/routes/product.js
+++ server/routes/product.js
@@ -18,6 +18,19 @@ const storage = multer.diskStorage({
   
 const upload = multer({ storage: storage }).single("file")
 
+router.post('/products', (req, res) => {
+
+    // products collection 에 들어 있는 모든 상품 정보 가져오기
+
+    Product.find()
+        .populate("writer") // writer 에 대한 모든 정보 가져오기
+        .exec((err, productInfo) => {
+            if (err) return res.status(400).json({ success: false, err })
+            return res.status(200).json({ success: true, productInfo })
+        })
+
+})
+
 router.post('/image', (req, res) => {
 
     // 가져온 이미지 저장

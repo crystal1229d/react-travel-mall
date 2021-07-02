1345e81ada4f6da1bee2ee5aefe0fc0c85d60812
crystal1229d <crystal1229d@naver.com>
2021-07-02 23:13:56 +0900

    add cartPage

---
+++ client/src/components/views/CartPage/CartPage.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getCartItems } from '../../../_actions/user_actions'
+
+function CartPage(props) {
+    
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        let cartItems= []
+
+        // 리덕스 User state 안에 cart 안에 상품이 들어있는지 확인
+        if (props.user.userData && props.user.userData.cart) {
+            // (if 문 검사 순서 : userData 가 있는가? 있다면 계속해서 userData.cart 가 있는가 검사. 전자가 없다면 false)
+            if (props.user.userData.cart.length > 0) {
+                props.user.userData.cart.forEach(item => {
+                    cartItems.push(item.id)
+                })
+
+                dispatch(getCartItems(cartItems, props.user.userData.cart))
+            }
+        }
+
+    })
+
+    return (
+        <div>
+            CartPage
+        </div>
+    )
+}
+
+export default CartPage

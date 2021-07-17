ee76005962ec2ca790185e77affd6b9d6c1a031a
crystal1229d <crystal1229d@naver.com>
2021-07-17 14:52:04 +0900

    add a feature which calculates total amount of products in cart to CartPage

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { getCartItems } from '../../../_actions/user_actions'
 import UserCardBlock from './Sections/UserCardBlock';
 
 function CartPage(props) {
+
+    const [Total, setTotal] = useState(0)
     
     const dispatch = useDispatch();
 
@@ -20,10 +22,21 @@ function CartPage(props) {
                 })
 
                 dispatch(getCartItems(cartItems, props.user.userData.cart))
+                    .then(response => {calculateTotal(response.payload)})
             }
         }
 
     }, [props.user.userData])
+
+    const calculateTotal =(cartDetail) => {
+        let total = 0;
+
+        cartDetail.map(item => {
+            total += parseInt(item.price, 10) * item.quantity
+        })
+
+        setTotal(total)
+    }
 
     return (
         <div style={{ width:'85%', margin:'3rem auto' }}>
@@ -32,6 +45,10 @@ function CartPage(props) {
 
             <div>
                 <UserCardBlock products={props.user.cartDetail} />
+            </div>
+
+            <div style={{ marginTop:'3rem' }}>
+                <h2>Total Amount: ${Total}</h2>
             </div>
 
         </div>
--- client/src/components/views/CartPage/Sections/UserCardBlock.js
+++ client/src/components/views/CartPage/Sections/UserCardBlock.js
@@ -11,8 +11,8 @@ function UserCardBlock(props) {
     }
 
     const renderItems = () => (
-        props.products && props.products.map(product => (
-            <tr key={product._id}>
+        props.products && props.products.map((product, index) => (
+            <tr key={index}>
                 <td>
                     <img 
                         style={{ width:'70px' }}

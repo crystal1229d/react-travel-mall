bbf3d1f0b48cced37feb23328a47eef7f4e97d4c
crystal1229d <crystal1229d@naver.com>
2021-07-18 22:19:45 +0900

    edit RemoveFromCart button

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { getCartItems, removeCartItem } from '../../../_actions/user_actions'
 import UserCardBlock from './Sections/UserCardBlock';
+import { Empty } from 'antd';
 
 function CartPage(props) {
 
     const [Total, setTotal] = useState(0)
+    const [ShowTotal, setShowTotal] = useState(false)
     
     const dispatch = useDispatch();
 
@@ -36,12 +38,15 @@ function CartPage(props) {
         })
 
         setTotal(total)
+        setShowTotal(true)
     }
 
     let removeFromCart = (productId) => {
         dispatch(removeCartItem(productId)) // dispatch 
         .then(response => {
-            
+            if (response.payload.productInfo.length <= 0) {
+                setShowTotal(false)            
+            }
         })
     }
 
@@ -54,9 +59,18 @@ function CartPage(props) {
                 <UserCardBlock products={props.user.cartDetail} removeItem={removeFromCart} />
             </div>
 
-            <div style={{ marginTop:'3rem' }}>
-                <h2>Total Amount: ${Total}</h2>
-            </div>
+            { ShowTotal ?
+                <div style={{ marginTop:'3rem' }}>
+                    <h2>Total Amount: ${Total}</h2>
+                </div>
+                :
+                <>
+                    <br />
+                    <Empty description={false} />
+                </>
+            }
+
+
 
         </div>
     )
